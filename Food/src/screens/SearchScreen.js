bd360307abb49167462bb3a4f543785f936bca16
Tony Turetsky <tturetsky@gmail.com>
2020-04-27 15:14:52 -0400

    started search bar

--- Food/src/screens/SearchScreen.js
+++ Food/src/screens/SearchScreen.js
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import Searchbar from './components/Searchbar';
 
 const SearchScreen = () => {
 	return (
-		<View>
+		<View style={styles.viewOne}>
 			<Text>Search Screen</Text>
+			<Searchbar />
 		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	viewOne: {
+		backgroundColor: '#110000',
+	},
+});
 
 export default SearchScreen;
---
+++ Food/src/screens/components/Searchbar.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Searchbar = () => {
+	return (
+		<View>
+			<Text>Searchbar</Text>
+		</View>
+	);
+};
+
+const styles = StyleSheet.create({});
+
+export default Searchbar;
